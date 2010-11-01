d0ffe5d7b5324280114094853a2fd506e90872bc
onedayitwillmake <mariogonzalez@gmail.com>
2010-11-01 03:05:53 -0400

    Added NetChannel stub.

---
+++ client/NetChannel.js
@@ -0,0 +1,17 @@
+/**
+File:
+	NetChannel
+Created By:
+	Mario Gonzalez
+Project:
+	Ogilvy Holiday Card 2010
+Abstract:
+	-> client talks to this object
+	 <-- this object talks to the server
+	  <--> server does some stuff
+	     -> the server talks to this object
+		  -> this object talks to the client --^
+	  			 	 
+Basic Usage: 
+
+*/
