dfba93d855867169b290686a63a86091be649775
onedayitwillmake <mariogonzalez@gmail.com>
2010-11-03 20:49:20 -0400

    Switched to use Vector class

--- client/js/CharacterController.js
+++ client/js/CharacterController.js
@@ -1,30 +1,27 @@
 var init = function()
 {
-	// some defaults we use
-	this.velocity = { x: 0, y: 0 }; // how fast i am going
-	this.acceleration = { x: .1, y: .1 }; // how much i accelerate per tick
-	this.damping = { x: .033, y: .033 }; // how much of a slide there is, how much do i take away per frame if there's no positive movement
-	this.maxVelocity = { x: 2.5, y: 2.5 };  // the fastest i can go
-	this.rotation = 0; // we start pointing up, simply easy b/c of sprites right now
-	this.aClientID = -1;
-	
-	var CharacterController = this.CharacterController =  function(aClientID) {
-		this.clientID = aClientID;
-		console.log('Character ID:', this.clientID);
-	};
-	
-	CharacterController.prototype.doSomething = function() {
-		console.log('(Testclass) doSomething()!');
-		return 'hi';
-	}
-	
-	return CharacterController;
-};
-
+	console.log(arguments);
+	return Class.extend({
+		init: function(aClientID) 
+		{
+			// some defaults we use
+			this.velocity = new Vector(0,0); // how fast i am going
+			this.acceleration = new Vector(0.1, 0.1); // how much i accelerate per tick
+			this.damping = new Vector(0.33,0.33); // how much of a slide there is, how much do i take away per frame if there's no positive movement
+			this.maxVelocity = new Vector(2.5, 2.5);  // the fastest i can go
+			this.rotation = 0; // we start pointing up, simply easy b/c of sprites right now
+			this.clientID = aClientID;
+		}	
+	});
+}
 
 if (typeof window === 'undefined') {
+	// We're in node!
 	var sys = require("sys");
-	exports.CharacterController = init();
+	var Vector = require('./lib/Vector').Class;
+	exports.Class = init();
 } else {
-	define(init);
+	// We're on the browser. 
+	// Require.js will use this file's name (CharacterController.js), to create a new  
+	define(['lib/Class'], init);
 }
