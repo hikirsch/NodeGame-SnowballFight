19820f458e0f0190646991edf338e66eefb7dc71
onedayitwillmake <mariogonzalez@gmail.com>
2010-11-03 20:50:21 -0400

    Cleaned up old commented-out code

--- server/ServerGameController.js
+++ server/ServerGameController.js
@@ -7,31 +7,35 @@ Project:
 	Ogilvy Holiday Card 2010
 Abstract:
 	This class is incharge of the actual game, it is aware of all the players / objects that currently exist.
-	The idea is to decouple this logic from the message-sending / receiving logic 
+	The servers version of the game, 'checks' all clients movements and internally handles collisions and informs clients
 Basic Usage: 
-	var ArgHelper 	= require('./argHelper.js');
-	var movespeed = ArgHelper.getArgumentByNameOrSetDefault(movespeed, 3.0); // returns 3.0
-	
-	// ...inside some other file
-	var movespeed = ArgHelper.getArgumentByNameOrSetDefault(movespeed, 1000); // returns 3.0
+	var gameController = new ServerGameController({
+	    'port': Math.abs(ArgHelper.getArgumentByNameOrSetDefault('port', 28785)),
+	    'status': false,
+	    'recordFile': './../record[date].js',
+	    'record': false,
+	    'server': null
+	});
+	gameController.run();
 	
 Version:
 	1.0
 */
 
 var sys = require('sys');
-var ArgHelper 	= require('./argHelper.js');
-var Snowball	= require('./snowball.game.js');
-var AbstractClassControllerClass = require('./../client/js/AbstractGameController.js').AbstractClassController;
+var ArgHelper 	= require('./lib/ArgHelper.js');
+var AbstractClassController = require('./../client/js/AbstractGameController.js').Class;
+require('./ServerNetChannel.js');
+
 (function(){
-	this.ServerGameController = AbstractClassControllerClass.extend({
+	this.ServerGameController = AbstractClassController.extend({
 		init: function(aHost, aPort)
 		{
 			this._super(aHost, aPort);
 			this.super = this._super;
-			
+
 			// Server
-			this.server = new Snowball.Server(this,
+			this.server = new ServerNetChannel(this,
 			{
 			    'port': Math.abs(ArgHelper.getArgumentByNameOrSetDefault('port', 28785)),
 			    'status': false,
@@ -52,47 +56,3 @@ var AbstractClassControllerClass = require('./../client/js/AbstractGameControlle
 		},
 	});
 })();
-//})();
-//
-//return AbstractGameController.extend({
-//	init: function(aHost, aPort) {
-//		console.log("Super:", this._super);
-//		this._super(aHost, aPort);
-//		this.super = this._super;
-//		 
-//		this.netChannel = new NetChannel(aHost, aPort, this);				
-//		this.view = new GameView();
-//	},
-//	tick: function()
-//	{
-//		this._super();
-//	},
-//	
-//	/**
-//	* These methods When netchannel recieves and validates a message
-//	* Anything we receive we can assume is valid
-//	**/
-//	netChannelDidConnect: function ()
-//	{
-//		 Good to go! Do some view setup.
-//		console.log("GameController.prototype.netChannelDidConnect");
-//	},
-//	
-//	netChannelDidReceiveMessage: function (messageEvent)
-//	{
-//		console.log('GameController.prototype.netChannelDidReceiveMessage');
-//	},
-//	
-//	netChannelDidDisconnect: function ()
-//	{
-//		console.log('GameController.prototype.netChannelDidDisconnect');
-//	},
-//});
-
-///var sys 		= require('sys');
-//var ArgHelper 	= require('./argHelper.js');
-//var Snowball	= require('./snowball.game.js');
-//AbstractGameController = require('./../client/js/AbstractGameController.js');
-//console.log(__dirname);
-//require('./../client/js/CharacterController.js');
-//exports.ServerGameController = ServerGameController;
