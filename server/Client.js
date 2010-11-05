9e6ca2a0dac76acc3d49444a2615aa53f92cd078
onedayitwillmake <mariogonzalez@gmail.com>
2010-11-05 02:37:56 -0400

    Added documentation.

--- server/Client.js
+++ server/Client.js
@@ -1,3 +1,24 @@
+/**
+File:
+	Client.js
+Created By:
+	Mario Gonzalez
+Project	:
+	Ogilvy Holiday Card 2010
+Abstract:
+	Stores information about a connection to a client
+Basic Usage: 
+	var aNewClient = new Client(this, connection, false);
+	
+	// Add to our list of connected users
+	this.clients[clientID] = aNewClient;
+	
+	// broadcast message to all clients
+	for( var clientID in this.clients ) {
+		this.clients[clientID].sendMessage(encodedMessage);
+	}
+	
+*/
 (function(){
 	this.Client = Class.extend(
 	{
