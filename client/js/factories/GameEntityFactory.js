3188786c21f5341ac376dddb4e67ff893c686c8a
onedayitwillmake <mariogonzalez@gmail.com>
2010-12-04 20:31:58 -0500

    Setting collision bitfield on entity creation

--- client/js/factories/GameEntityFactory.js
+++ client/js/factories/GameEntityFactory.js
@@ -28,19 +28,31 @@ var init = function(Vector, Rectangle, GameEntity, Character, ClientControlledCh
 			this.entityTypes.setObjectForKey(Character, 'Character');
 			this.entityTypes.setObjectForKey(ClientControlledCharacter, 'ClientControlledCharacter');
 			this.entityTypes.setObjectForKey(Projectile, 'Projectile');
+
+			this.collisionBitmask = {
+				'Character':  1 << 0,
+				'Projectile': 1 << 1,
+				'CollidableLevelObject': 1 << 2
+			};
+
 		},
 
 		createProjectile: function(anObjectID, aClientID, aProjectileModel, aFieldController)
 		{
 			//this.entityTypes.objectForKey(aCharacterType); // Retrieve class from sorted table
 			var projectile = new Projectile(anObjectID, aClientID, aFieldController, aProjectileModel, 1);
+			projectile.collisionBitfield = this.collisionBitmask.Character & this.collisionBitmask.CollidableLevelObject;
+			
 			return projectile;
 		},
 
 		createCharacter: function(anObjectID, aClientID, aCharacterType, aFieldController)
 		{
-			var characterClass = this.entityTypes.objectForKey(aCharacterType); // Retrieve class from sorted table
-			return new characterClass(anObjectID, aClientID, aFieldController);
+			var characterClass = this.entityTypes.objectForKey(aCharacterType), // Retrieve class from sorted table
+				aNewCharacter = new characterClass(anObjectID, aClientID, aFieldController);
+			aNewCharacter.collisionBitfield = this.collisionBitmask.Projectile & this.collisionBitmask.CollidableLevelObject;
+
+			return aNewCharacter;
 		}
 	});
 };
@@ -49,13 +61,13 @@ if (typeof window === 'undefined')
 {
 	// We're in node!
 	require('../lib/jsclass/core.js');
-	require('../lib/Rectangle');
 	require('../lib/Vector');
+	require('../lib/Rectangle');
 	require('../controllers/entities/GameEntity');
 	require('../controllers/entities/ClientControlledCharacter');
 	require('../controllers/entities/Character');
 	require('../controllers/entities/Projectile');
-	var sys = require('sys');
+	
 	GameEntityFactory = init(Vector, Rectangle, GameEntity, Character, ClientControlledCharacter, Projectile);
 }
 else
