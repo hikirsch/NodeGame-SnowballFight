412b22b7edfe480b423631087edb1274a28351c7
onedayitwillmake <mariogonzalez@gmail.com>
2010-12-07 22:25:07 -0500

    Adding Projectile traits

---
+++ client/js/controllers/entities/traits/ProjectileTraitFreeze.js
@@ -0,0 +1,56 @@
+/**
+File:
+	ClientControlledTrait.js
+Created By:
+	Mario Gonzalez
+Project	:
+	Ogilvy Holiday Card 2010
+Abstract:
+	Gives a 'Character' the ability to be controlled by the keyboard.
+Basic Usage:
+
+ 	// Let my character be controlled by the KB
+	if(newEntity.connectionID === this.netChannel.connectionID) {
+		aCharacter = new ClientControlledTrait(aCharacter);
+		aCharacter.setInput( new Joystick() );
+		this.clientCharacter = aCharacter;
+	}
+*/
+
+var init = function(Character)
+{
+	return new JS.Decorator(Character,
+	{
+		effectEntity: function( entity )
+		{
+//			this.component.setInput( joystick );
+//			this.component.input.attachEvents();
+		},
+
+		constructEntityDescription: function()
+		{
+			this.component.input.constructInputBitmask();
+			return {
+				objectID: this.component.objectID,
+				clientID: this.component.clientID,
+				input: this.component.input.constructInputBitmask()
+			}
+		},
+
+		// Catch the handleInput so our super's version doesn't get called
+		handleInput: function( gameClock ) {}
+	});
+};
+
+if (typeof window === 'undefined')
+{
+	// We're in node!
+	require('./Character');
+	ProjectileTraitFreeze = init(Character);
+}
+else
+{
+	// We're on the browser.
+	// Require.js will use this file's name (CharacterController.js), to create a new
+	define(['controllers/entities/Character', 'lib/jsclass/core', 'lib/jsclass/decorator'], init);
+}
